39b46d32a8d7664cd013dc931fac9867b7c0dbfc
facuspagnoletta <101607658+facuspagnoletta@users.noreply.github.com>
2025-04-23 23:58:51 -0300

    Add files via upload

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,20 @@
-# Aplicacion del servidor
+from microdot import Microdot
+from microdot import send_file
+
+app = Microdot()
+
+
+@app.route('/')
+def index(request):
+    return send_file('index.html', content_type='text/html')  # in seconds
+
+@app.route('/styles/base.css')
+def css(request):
+    return send_file('styles/base.css', content_type='text/css')  # in seconds
+
+@app.route('/scripts/base.js')
+def css(request):
+    return send_file('scripts/base.js', content_type='text/css')  # in seconds
+
+
+app.run(debug=True)
